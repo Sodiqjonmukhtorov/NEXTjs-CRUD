9ba49d1367268bd98760ffa757615a15573b903a
Sodiqjonmukhtorov <sodiqjonm16@gmail.com>
2024-09-08 17:46:32 +0500

    jnndnjd

---
+++ src/app/Respect/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Respect = () => {
+  return (
+    <h1>Respect</h1>
+  )
+}
+
+export default Respect
---
+++ src/app/products/page.js
@@ -0,0 +1,8 @@
+// Make sure it looks something like this:
+const Products = () => {
+  return (
+      <h1>Products</h1>
+  );
+};
+
+export default Products;
